ee65f03d44f425536b69909226af62d18c31c959
Elías Ramírez Hernández <168288076+EliPoli64@users.noreply.github.com>
2025-11-12 19:38:55 -0600

    updated: script de llenado con tipos evento

--- scripts/ScriptLlenado.sql
+++ scripts/ScriptLlenado.sql
@@ -3,7 +3,7 @@ USE ServiciosDB;
 INSERT INTO dbo.TipoMovConsumo (
 	[ID]
 	, [Nombre]
-	) VALUES
+) VALUES
 	(1, 'Debito por lectura')
 	, (2, 'Credito por ajuste')
 	, (3, 'Debito por ajuste');
@@ -11,7 +11,7 @@ INSERT INTO dbo.TipoMovConsumo (
 INSERT INTO dbo.TipoUsoPropiedad (
 	[ID]
 	, [Nombre]
-	) VALUES
+) VALUES
 	(1, 'Habitacion')
 	, (2, 'Comercial')
 	, (3, 'Industrial')
@@ -21,7 +21,7 @@ INSERT INTO dbo.TipoUsoPropiedad (
 INSERT INTO dbo.TipoAreaPropiedad (
 	[ID]
 	, [Nombre]
-	) VALUES
+) VALUES
 	(1, 'Residencial')
 	, (2, 'Agricola')
 	, (3, 'Bosque')
@@ -31,21 +31,21 @@ INSERT INTO dbo.TipoAreaPropiedad (
 INSERT INTO dbo.TipoUsuario(
 	[ID]
 	, [Nombre]
-	) VALUES
+) VALUES
 	(1, 'Administrador')
 	, (2, 'Propietario');
 
 INSERT INTO dbo.TipoAsociacion(
 	[ID]
 	, [Nombre]
-	) VALUES
+) VALUES
 	(1, 'Asociar')
 	, (2, 'Desasociar');
 
 INSERT INTO dbo.TipoMedioPago (
 	[ID]
 	, [Nombre]
-	) VALUES
+) VALUES
 	(1, 'Efectivo')
 	, (2, 'Tarjeta');
 
@@ -53,10 +53,30 @@ INSERT INTO dbo.CCPeriodoCobro (
 	[ID]
 	, [Nombre]
 	, [QDividir]
-	) VALUES
+) VALUES
 	(1, 'Mensual', 1)
 	, (2, 'Trimestral', 3)
 	, (3, 'Semestral', 6)
 	, (4, 'Anual', 12)
 	, (5, 'Unico no recurrente', 1)
 	, (6, 'Diario intereses moratorios', 30);
+
+INSERT INTO dbo.TipoEvento (
+	[ID]
+	, [Nombre]
+) VALUES
+	(1, 'Consulta propiedad')
+	, (2, 'Consulta factura')
+	, (3, 'Calculo moratorios')
+	, (4, 'Pago factura')
+	, (5, 'Generacion factura')
+	, (6, 'Lectura medidor')
+	, (7, 'Orden corte agua')
+	, (8, 'Orden reconexion')
+	, (9, 'Asociacion propiedad')
+	, (10, 'Cambio valor propiedad')
+	, (11, 'Error sistema')
+	, (12, 'Login usuario')
+	, (13, 'Logout usuario')
+	, (14, 'Modificacion datos')
+	, (15, 'Proceso masivo');
