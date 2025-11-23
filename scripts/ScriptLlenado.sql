bf598613a8609ed3ae307db60f98a35d691f8eaf
Elías Ramírez Hernández <168288076+EliPoli64@users.noreply.github.com>
2025-11-23 08:51:14 -0600

    ajustes al script de llenado

--- scripts/ScriptLlenado.sql
+++ scripts/ScriptLlenado.sql
@@ -1,13 +1,47 @@
 USE ServiciosDB;
 
-INSERT INTO dbo.Usuario (
-	[ID]
-	, UserName
-	, [Password]
-	, EsActivo
-	, IDTipo
-) VALUES
-	(1, 'elipoli', '12341234', 1, 1);
+EXEC sp_msforeachtable "ALTER TABLE ? NOCHECK CONSTRAINT all"
+DELETE FROM [dbo].[AsociacionPxP]
+DELETE FROM [dbo].[CCAgua]
+DELETE FROM [dbo].[CCTarifaFija]
+DELETE FROM [dbo].[CCTarifaPorcentual]
+DELETE FROM [dbo].[CCTarifaTramo]
+DELETE FROM [dbo].[ComprobantePago]
+DELETE FROM [dbo].[DBError]
+DELETE FROM [dbo].[Error]
+DELETE FROM [dbo].[Linea]
+DELETE FROM [dbo].[MovConsumo]
+DELETE FROM [dbo].[OrdenReconexion]
+DELETE FROM [dbo].[OrdenCorte]
+DELETE FROM [dbo].[Factura]
+DELETE FROM [dbo].[PropiedadXCC]
+DELETE FROM [dbo].[CCTarifa]
+DELETE FROM [dbo].[Propiedad]
+DELETE FROM [dbo].[Propietario]
+DELETE FROM [dbo].[UsuarioPropietario]
+DELETE FROM [dbo].[Usuario]
+DELETE FROM [dbo].[TipoUsuario]
+DELETE FROM [dbo].[TipoUsoPropiedad]
+DELETE FROM [dbo].[TipoMovConsumo]
+DELETE FROM [dbo].[TipoMedioPago]
+DELETE FROM [dbo].[TipoEvento]
+DELETE FROM [dbo].[TipoAsociacion]
+DELETE FROM [dbo].[TipoAreaPropiedad]
+DELETE FROM [dbo].[ParametrosSistema]
+DELETE FROM [dbo].[ConceptoCobro]
+DELETE FROM [dbo].[CCTipoMonto]
+DELETE FROM [dbo].[CCPeriodoCobro]
+DELETE FROM [dbo].[CCBaseCalculo]
+DELETE FROM [dbo].[Bitacora]
+DBCC CHECKIDENT ('[dbo].[Bitacora]', RESEED, 0)
+DBCC CHECKIDENT ('[dbo].[ComprobantePago]', RESEED, 0)
+DBCC CHECKIDENT ('[dbo].[DBError]', RESEED, 0)
+DBCC CHECKIDENT ('[dbo].[Factura]', RESEED, 0)
+DBCC CHECKIDENT ('[dbo].[Linea]', RESEED, 0)
+DBCC CHECKIDENT ('[dbo].[MovConsumo]', RESEED, 0)
+DBCC CHECKIDENT ('[dbo].[OrdenCorte]', RESEED, 0)
+DBCC CHECKIDENT ('[dbo].[OrdenReconexion]', RESEED, 0)
+EXEC sp_msforeachtable "ALTER TABLE ? WITH CHECK CHECK CONSTRAINT all"
 
 INSERT INTO dbo.ParametrosSistema (
 	ID
@@ -17,6 +51,27 @@ INSERT INTO dbo.ParametrosSistema (
 	(1, 'DiasVencimientoFactura', '15')
 	, (2, 'DiasCorteAgua', '10')
 ;
+
+INSERT INTO dbo.TipoEvento (
+	[ID]
+	, [Nombre]
+) VALUES
+	(1, 'Consulta propiedad')
+	, (2, 'Consulta factura')
+	, (3, 'Calculo moratorios')
+	, (4, 'Pago factura')
+	, (5, 'Generacion factura')
+	, (6, 'Lectura medidor')
+	, (7, 'Orden corte agua')
+	, (8, 'Orden reconexion')
+	, (9, 'Asociacion propiedad')
+	, (10, 'Cambio valor propiedad')
+	, (11, 'Error sistema')
+	, (12, 'Login usuario')
+	, (13, 'Logout usuario')
+	, (14, 'Modificacion datos')
+	, (15, 'Proceso masivo');
+
 
 DECLARE @xml XML;
 
@@ -58,6 +113,15 @@ SELECT
 	TipoUser.value('@id', 'INT')
 	, TipoUser.value('@nombre', 'VARCHAR(32)')
 FROM @xml.nodes('/Catalogos/TipoUsuario/TipoUser') AS T(TipoUser);
+
+INSERT INTO dbo.Usuario (
+	[ID]
+	, UserName
+	, [Password]
+	, EsActivo
+	, IDTipo
+) VALUES
+	(1, 'elipoli', '12341234', 1, 1);
 
 INSERT INTO dbo.TipoAsociacion (
 	[ID]
@@ -125,6 +189,15 @@ SELECT
 	, '2099-12-31'
 FROM @xml.nodes('/Catalogos/CCs/CC') AS T(CC);
 
+INSERT INTO dbo.CCBaseCalculo (
+	[ID]
+	, [Nombre]
+) VALUES
+	(1, 'Valor propiedad')
+	, (2, 'Area propiedad')
+	, (3, 'Consumo agua')
+	, (4, 'Monto factura');
+
 INSERT INTO dbo.CCAgua (
 	[ID]
 	, [IDTarifa]
@@ -174,13 +247,12 @@ FROM @xml.nodes('/Catalogos/CCs/CC') AS T(CC)
 WHERE CC.value('@TipoMontoCC', 'INT') = 3 
 AND CC.value('@ValorPorcentual', 'VARCHAR(32)') != '';
 
-
 DECLARE @OperacionesXml XML;
 
 SELECT @OperacionesXml = BulkColumn
 FROM OPENROWSET(BULK 'C:\Users\Elias\projs\Tarea3BasesDatos\XMLs\xmlUltimo.xml', SINGLE_BLOB) AS x;
 
-DECLARE @inUserName VARCHAR(32) = 'admin';
+DECLARE @inUserName VARCHAR(32) = 'elipoli';
 DECLARE @inIP VARCHAR(32) = '127.0.0.1';
 DECLARE @outResultCode INT;
 
@@ -193,6 +265,7 @@ DECLARE @FechasOperacion TABLE (
 	, CCPropiedadXml XML
 	, LecturasMedidorXml XML
 	, PagosXml XML
+    , ValorXml XML
 );
 
 INSERT INTO @FechasOperacion (
@@ -203,6 +276,7 @@ INSERT INTO @FechasOperacion (
 	, CCPropiedadXml
 	, LecturasMedidorXml
 	, PagosXml
+    , ValorXml
 )
 SELECT
 	T.FechaOperacion.value('@fecha', 'DATE')
@@ -212,8 +286,8 @@ SELECT
 	, T.FechaOperacion.query('./CCPropiedad')
 	, T.FechaOperacion.query('./LecturasMedidor')
 	, T.FechaOperacion.query('./Pagos')
+    , T.FechaOperacion.query('./PropiedadCambio')
 FROM @OperacionesXml.nodes('/Operaciones/FechaOperacion') AS T(FechaOperacion);
-
 DECLARE @Contador INT = 1;
 DECLARE @TotalFechas INT = (SELECT COUNT(*) FROM @FechasOperacion);
 DECLARE @FechaOperacion DATE;
@@ -229,6 +303,22 @@ BEGIN
 	BEGIN
 		EXEC dbo.XMLProcesarPersonas
 			@Xml = @PersonasXml
+			, @inUserName = @inUserName
+			, @inIP = @inIP
+			, @outResultCode = @outResultCode OUTPUT;
+		IF (@outResultCode != 0)
+		BEGIN
+			SELECT @outResultCode, @inUserName, @inIP;
+			RETURN;
+		END;
+	END;
+
+    DECLARE @ValorXml XML = (SELECT ValorXml FROM @FechasOperacion WHERE ID = @Contador);
+    
+	IF @ValorXml IS NOT NULL AND @ValorXml.exist('*') = 1
+	BEGIN
+		EXEC dbo.XMLProcesarPropiedadCambioValor
+			@Xml = @ValorXml
 			, @inUserName = @inUserName
 			, @inIP = @inIP
 			, @outResultCode = @outResultCode OUTPUT;
@@ -278,7 +368,7 @@ BEGIN
 			, @inUserName = @inUserName
 			, @inIP = @inIP
 			, @outResultCode = @outResultCode OUTPUT;
-		IF (@outResultCode != 0)
+		IF (@outResultCode != 0 AND @outResultCode != 50004)
 		BEGIN
             SELECT @outResultCode, 'CCProp';
 			RETURN;
@@ -301,6 +391,8 @@ BEGIN
 		END;
 	END;
 
+    EXEC MasivoGenerarFacturasDelDia @FechaOperacion, @inUserName, '127.0.0.0', @outResultCode;
+
 	DECLARE @PagosXml XML = (SELECT PagosXml FROM @FechasOperacion WHERE ID = @Contador);
 	IF @PagosXml IS NOT NULL AND @PagosXml.exist('*') = 1
 	BEGIN
@@ -316,6 +408,8 @@ BEGIN
 			RETURN;
 		END;
 	END;
+    EXEC MasivoGenerarCortes @FechaOperacion, @inUserName, '127.0.0.0', @outResultCode;
+    EXEC MasivoGenerarReconexiones @FechaOperacion, @inUserName, '127.0.0.0', @outResultCode;
 
 	SET @Contador = @Contador + 1;
 END;
